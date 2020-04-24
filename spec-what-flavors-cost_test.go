d7ad86917b1b66120c3102c0765277524d9d2130
John McKee <jmckee3@mac.com>
2020-04-24 10:03:22 -0700

    initial commit

---
+++ spec-what-flavors-cost_test.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestWhatFlavorsCost1(t *testing.T) {
+cost := int32[1,4,5,3,2]
+money := int32(4)
+
+whatFlavors(cost []int32, money int32)
+
+	// if returnedString != "" {
+	// 	t.Errorf("got %s instead of ", returnedString)
+	// }
+}
+
---
+++ what-flavors-cost.go
@@ -0,0 +1,7 @@
+package cost
+
+func whatFlavors(cost []int32, money int32) {
+
+}
+
+func main() {}
